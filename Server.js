ccb81eb48008141b920c40b8145414b98f8f2ecc
areeven <emilie.ost1990@gmail.com>
2021-09-02 14:01:49 +0200

    added user print by id

--- Server.js
+++ Server.js
@@ -7,24 +7,28 @@ const port = process.env.PORT
 
 let user_database = [
     {
+        id: 10,
         name: 'Adam',
         age: 12,
-        gender: 'M'
+        gender: 'Male'
     },
     {
+        id: 11,
         name: 'Bengt',
         age: 23,
-        gender: 'M'
+        gender: 'Male'
     },
     {
+        id: 12,
         name: 'Gertrud',
         age: 45,
-        gender: 'F'
+        gender: 'Female'
     },
     {
+        id: 13,
         name: 'Dana',
         age: 20,
-        gender: 'F'
+        gender: 'Female'
     }
 ]
 
@@ -32,10 +36,28 @@ let user_database = [
 function returnUserNames() {
     let text = ''
     user_database.forEach(item => {
-        text += item.name + ', '
-    });
+        text += item.name + ' id=' + item.id + ', <br>'
+    })
     return text
 }
+
+// Tar endast emot nummer!
+function getUserById(id) {
+    let text = `User with id = ${id} `
+
+    for (const item of user_database) {
+        if (id === item.id) {
+            text += `is <br>${item.name} with the age of = ${item.age} and gender ${item.gender}<br>`
+            return text
+        }
+    }
+
+    text += "<br>Doesn't exist in the database"
+
+    return text
+}
+
+
 
 app.get('/', function (req, res) {
     res.send('API is alive!')
@@ -43,7 +65,13 @@ app.get('/', function (req, res) {
 
 app.get('/users', (req, res) => {
     let returnText = returnUserNames()
-    res.send(`Available users: ${returnText}!`)
+    res.send(`Available users: <br>${returnText}!`)
+})
+
+app.get('/users/:id', (req, res) => {
+    const id = Number(req.params.id)
+    let returnText = getUserById(id)
+    res.send(returnText)
 })
 
 app.listen(port, () => {
