5b891403e5cc156d27fac7c481783728164f8132
areeven <emilie.ost1990@gmail.com>
2021-09-02 13:07:58 +0200

    added 'database'

--- Server.js
+++ Server.js
@@ -5,10 +5,33 @@ dotenv.config()
 const app = express()
 const port = process.env.PORT
 
+let user_database = [
+    {
+        name: 'Adam',
+        age: 12,
+        gender: 'M'
+    },
+    {
+        name: 'Bengt',
+        age: 23,
+        gender: 'M'
+    },
+    {
+        name: 'Gertrud',
+        age: 45,
+        gender: 'F'
+    },
+    {
+        name: 'Dana',
+        age: 20,
+        gender: 'F'
+    }
+]
+
+
 app.get('/', function (req, res) {
     res.send('API is alive!')
 })
-
 
 app.listen(port, () => {
     console.log(`Server is up and running on port ${port}`)
